caa4a878311aaa6f6bf4c4bd4fd80ed08941ced6
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-01-07 13:46:07 +0600

    Basic view set

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Mahmudul Hasan on 7/1/23.
+//
+
+import SwiftUI
+
+let columns : [GridItem] = [GridItem(.flexible()),
+                            GridItem(.flexible()),
+                            GridItem(.flexible()),]
+struct ContentView: View {
+    
+    var body: some View {
+        GeometryReader {geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns:columns, spacing: 5) {
+                    ForEach(0..<9) { i in
+                        ZStack{
+                            Circle()
+                                .foregroundColor(.red) .opacity(0.5).frame(width: geometry.size.width/3 - 15,
+                                    height: geometry.size.width/3 - 15)
+                            Image(systemName:"xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                            
+                            
+                                
+                            
+                            
+                        }
+                    }
+                }
+                Spacer()
+            } .padding()
+            
+        }
+        
+    }
+    
+  
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Mahmudul Hasan on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
